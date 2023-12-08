216bc57d443b17269930cf40c09c6d605402697c
hana <ansgksk97@naver.com>
2023-12-08 16:10:49 +0900

    그린그램3 디엠구현

---
+++ src/main/java/com/green/greengram3/dm/model/DmUserInsDto.java
@@ -0,0 +1,12 @@
+package com.green.greengram3.dm.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class DmUserInsDto {
+    private int idm;
+    private int iuser;
+
+}
